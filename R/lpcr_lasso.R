f0e5daa3a8c0f445f60bb05d096694ef924708bc
seonjoo <seonjool@gmail.com>
2016-04-27 00:30:45 -0400

    initial commit v1

--- R/lpcr_lasso.R
+++ R/lpcr_lasso.R
@@ -22,59 +22,71 @@
 #' @keywords hdlfpca glmnet
 #' @export
 
-lpcr_lasso <- function(Y,T=NULL,J=NULL,I=NULL,visit=NULL, lfpca=NULL, cov=NULL, Nx,nfold=10,lambdalist = 2^(c(-10:10)/2), penalty.factor=NULL, M=NULL){
+lpcr_lasso <- function(Y,T=NULL,J=NULL,I=NULL,visit=NULL, lfpca=NULL, cov=NULL,
+                       nfold=10,lambdalist = 2^(c(-10:10)/2), penalty.factor=NULL, M=NULL,
+                       seednum=1234){
   library(glmnet)
   ## If lfpca is has not been conducted, it will run.
   if (is.null(lfpca)){
     if (is.null(T) |  is.null(J) | is.null(I) | is.null(visit)){
-      print('LFPCA has not been run, and reuiqred parameters were not specified.')
+      print('LFPCA has not been run, and the required parameters for LFPCA were not specified.')
       break
     }else{
         system.time(lfpc<-hd_lfpca(Y,T,J,I,visit,varthresh = 0.85, timeadjust=FALSE, figure=TRUE))
     }
   }
 
-  X = lfpc$xi
+  if(is.null(ncol(cov))){
+    if(is.null(cov)) ncov=0
+    else{
+      ncov=1
+      }
+    }else{
+    ncov=ncol(cov)
+  }
+
+  rownames(lfpca$xi)<-paste('LPC',1:lfpca$Nx,sep='')
+  X = cbind(cov,t(lfpca$xi))
+  print(head(X))
+  Nx = lfpca$Nx
 
   if(is.null(penalty.factor)){
     if (is.null(cov)){
       penalty.factor=rep(1,ncol(X))
     }else{
-      if(is.null(ncol(cov))){
-        ncov=1
-      }else{
-        ncov=ncol(cov)
-      }
       penalty.factor=c(rep(0,ncov),rep(1,ncol(X)))
     }
   }
 
   ## lasso
-  fitcv=cv.glmnet(t(X),Y, family='binomial', alpha = 1, lambda = lambdalist, nfold=nfold, penalty.factor=penalty.factor)
-  fit = glmnet(t(X),Y, family='binomial', alpha = 1, lambda =fitcv$lambda.min, penalty.factor=penalty.factor)
-  coef <- coef(fit)
+  set.seed(seednum)
+  fitcv=cv.glmnet(X,Y, family='binomial', alpha = 1, lambda = lambdalist, nfold=nfold, penalty.factor=penalty.factor)
+  fit = glmnet(X,Y, family='binomial', alpha = 1, lambda =fitcv$lambda.min, penalty.factor=penalty.factor)
+  coefs <- coef(fit)
 
-  colnames(lfpca$phix0) <- paste('X', 1:lfpca$Nx, sep='')
-  colnames(lfpca$phix1) <- paste('X', 1:lfpca$Nx, sep='')
-  if (is.null(M)) {
-    rownames(X) <- paste('X', 1:lfpca$Nx, sep='')
-  } else {rownames(X)[-c(1:dim(M)[2])] <- paste('X', 1:lfpca$Nx, sep='')}
+  colnames(lfpca$phix0) <- paste('LPC', 1:lfpca$Nx, sep='')
+  colnames(lfpca$phix1) <- paste('LPC', 1:lfpca$Nx, sep='')
 
-  ## extract selected components
-  if (is.null(M)) {
-    logic <- (coef!=0)[-1]
-  } else{
-    logic <- (coef!=0)[-c(0:dim(M)[2]+1)]
-  }
-  phix0 = lfpca$phix0[,which(logic==1)]
-  phix1 = lfpca$phix1[,which(logic==1)]
-  Nx = sum(logic)
-  if (!is.null(M)) {X = X[-c(1:dim(M)[2]),]}
-  xi = X[which(logic==1),]
+  nonzeroindx =  which(coefs!=0)-1
+
+  nonzeroindx2 =  (coefs[-c(0:ncov +1)]!=0)
+## extract selected components
+#  if (is.null(M)) {
+#    logic <- (coef!=0)[-1]
+#  } else{
+#    logic <- (coef!=0)[-c(0:dim(M)[2]+1)]
+#  }
+
+  refit = glmnet(X[,nonzeroindx],Y, family='binomial', alpha = 1, lambda =0)
 
 
-  return(list(  xi = xi,
-                phix0 = phix0,
-                phix1 = phix1,
-                Nx = dim(xi)[1] ))
+  hattheta = as.matrix(coef(refit))
+
+  print(dim(lfpca$phix0[,nonzeroindx2]))
+  print(hattheta)
+  print(ncov)
+  beta0 = lfpca$phix0[,nonzeroindx2] %*% hattheta[-c(0:ncov +1)]
+  beta1 = lfpca$phix0[,nonzeroindx2] %*% hattheta[-c(0:ncov +1)]
+  return(list(nonzeroindx = nonzeroindx, nonzeroindx2 = nonzeroindx2,refit=refit, beta0=beta0,beta1=beta1,hattheta=hattheta))
+
 }
